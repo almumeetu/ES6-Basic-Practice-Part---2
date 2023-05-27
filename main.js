d3e2bfac753b5b2a14994d749d5cc57603ad6198
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-05-27 14:49:53 +0600

    problem 92 done

--- main.js
+++ main.js
@@ -13,7 +13,12 @@ console.log(two_digits(50))
 //     var all_chars = str.split("");
 //     for(var i = 0; i < all_chars.length; i++) {
 //       var n = all_chars[i].charCodeAt() - 'a'.charCodeAt();
-//     //   console.log(n);
+
+//       console.log(all_chars[i].charCodeAt());
+
+//       console.log('a'.charCodeAt());
+
+//       console.log(n);
 //       n = (n + 1) % 26; 
 //       all_chars[i] = String.fromCharCode(n + 'a'.charCodeAt());
 //       console.log(all_chars[i]);
@@ -22,6 +27,27 @@ console.log(two_digits(50))
 //   }
 
 //   console.log(alphabet_char_Shift("abcd"));
+
+function replaceCharWithNext(str) {
+   let newString = " ";
+   for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      let code = char.charCodeAt(0);
+
+      if (char >= "a" && char <= "z") {
+         code = ((code - 97 + 1) % 26) + 97;
+      } else if (char >= "A" && char <= "Z") {
+         code = ((code - 65 + 1) % 26) + 65;
+      }
+      newString += String.fromCharCode(code);
+   }
+   return newString;
+}
+console.log("Solution of the Problem #84");
+console.log(replaceCharWithNext("abcdxyz"));
+console.log(replaceCharWithNext("sad"));
+console.log(replaceCharWithNext("marriage"));
+console.log(replaceCharWithNext("zug"));
 
 // problem 86. Write a JavaScript program to find the types of a given angle.  
 
@@ -44,7 +70,8 @@ console.log(angle_Type(145))
 console.log(angle_Type(180))
 
 
-// 88. Write a JavaScript program that takes two integers and a divisor. If the given divisor divides both integers and does not divide either, two specified integers are similar. Check whether two integers are similar or not.
+// 88. Write a JavaScript program that takes two integers and a divisor. If the given divisor divides both integers and does not divide either,
+//  two specified integers are similar. Check whether two integers are similar or not.
 
 function checking_numbers(x, y, divisor) {
    if (x % divisor === 0 && y % divisor === 0 || x % divisor !== 0 && y % divisor !==
@@ -62,12 +89,32 @@ console.log(checking_numbers(10, 20, 4))
 // Problem 90. Write a JavaScript program to find the kth greatest element in a given array of integers. 
 
 function Kth_greatest_in_array(arr, k) {
-   const array = (x,y) => x - y;
+   const array = (x, y) => x - y;
    let a = arr.sort(array);
    return a[k - 1];
 }
 
 console.log(Kth_greatest_in_array([1, 2, 7, 4, 5], 3));
-console.log(Kth_greatest_in_array([3, 45, 2, 7, 9, 11], 1));
+console.log(Kth_greatest_in_array([3, 45, 2, 7, 9, 11], 3));
 
 
+
+// Problem 92. Write a JavaScript program to find the maximum difference between any two adjacent elements of a given array of integers.
+
+function max_difference(arr) {
+	var max = -1;
+    var temp;
+	for (var i = 0; i < arr.length - 1; i++)
+      {
+		temp = Math.abs(arr[i] - arr[i + 1]);
+      // console.log(temp);
+
+		max = Math.max(max, temp);
+      // console.log(max);
+	  }
+	return max;
+}
+
+console.log(max_difference([1, 2, 3, 8, 9]))
+// console.log(max_difference([1, 2, 3, 18, 9]))
+// console.log(max_difference([13, 2, 3, 8, 9]))
